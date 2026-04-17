e8d86e08aa5ef31d406d89857eeb3d4aa9019399
Stanislav Peresypkin <stas.peresypkin@mail.ru>
2026-04-17 19:21:47 +0300

    Done with Docker

--- obj/Debug/net8.0/MusicAlbums.AssemblyInfo.cs
+++ obj/Debug/net8.0/MusicAlbums.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicAlbums")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d3d39c61ca846ff4035855268cdef6c12bda123")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f106b504edea9547d5e42d74d54c3fed8276587d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicAlbums")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicAlbums")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
